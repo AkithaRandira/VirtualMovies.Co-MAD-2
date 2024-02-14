957a0a812923cdad80c2d1442c55a6cce3a343b2
AkithaRandira <akitharandira@gmail.com>
2024-02-14 22:36:40 +0530

    Final Project

--- app/src/main/java/com/example/myapplication/Activity/IntroActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/IntroActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
@@ -12,5 +15,13 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Button getinBtn=findViewById(R.id.getInbtn);
+        getinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
